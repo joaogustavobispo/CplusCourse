a0a48fe23cadf82ba867eab008e461bef2509603
João Gustavo <joao.bispo@uvvnet.com.br>
2020-09-16 13:18:41 -0300

    Add exercicios

---
+++ Exercicios/euvou.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// João Gustavo Soares Bispo
+
+/* Exibi na tela oque me foi pedido */
+int main(int argc, char const *argv[])
+{
+    std::cout << "Eu\nvou\naprender\nc++" << std::endl;
+    system("PAUSE");
+    return 0;
+}
