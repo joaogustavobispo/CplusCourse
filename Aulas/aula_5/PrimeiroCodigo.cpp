91f90e9d94cf4472ec6fe1e753e2929e4467ffb0
João Gustavo <joao.bispo@uvvnet.com.br>
2020-09-16 12:57:41 -0300

    Add código da aula 5

---
+++ Aulas/aula_5/PrimeiroCodigo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// Include - Diretiva de pré processador.
+// Indica para incluir o arquivo de cabeçalho "iostream"
+
+int main()
+// Responsável por inicializar o programa
+{ // Inicia um bloco de código
+
+    //cout - comando de saida de dados
+    std::cout << "Hello World" << std::endl;
+    system("echo '---fim do código---'");
+    return 0;
+} // Fecha um bloco de código
